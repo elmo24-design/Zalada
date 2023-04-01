23d64c4d787a92bbce72ff529fc5eccea80bbc22
elmo24-design <mahupil24@gmail.com>
2023-04-01 20:04:01 +0800

    2nd commit
    Added cloudinary for image upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import multer from 'multer'
 const router = express.Router()
 import path from 'path'
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary';
 
 const storage = multer.diskStorage({
    destination(req, file, cb){
@@ -31,8 +32,32 @@ const upload = multer({
    }
 })
 
-router.post('/', upload.single('image'), (req, res) => {
-   res.send(`/${req.file.path}`)
+// configure cloudinary
+cloudinary.config({
+   cloud_name: 'day58jujg',
+   api_key: '341172781363147',
+   api_secret: '5xWMIrzD4744TedSBV6PmGj_n04',
+});
+
+router.post('/', upload.single('image'), async(req, res) => {
+   // res.send(`/${req.file.path}`)
+   try {
+      // upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+  
+      // res.status(201).json({
+      //   message: 'Image uploaded successfully',
+      //   imageUrl: result.secure_url,
+      // });
+
+      res.send(result.secure_url)
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: 'Failed to upload image',
+      });
+    }
 })
 
 export default router
